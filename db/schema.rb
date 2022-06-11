81b2a6f748bd5172155a11ac3b111a793bbcd5e1
tail1290 <oimo0921@yahoo.co.jp>
2022-06-11 19:32:30 +0900

    プルダウン実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_11_075600) do
+ActiveRecord::Schema.define(version: 2022_06_11_080215) do
+
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "room", null: false
+    t.string "user", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
