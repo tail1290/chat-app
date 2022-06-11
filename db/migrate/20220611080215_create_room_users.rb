015dd6f678116828d1a6ad026e8e921d8aaaa1ac
tail1290 <oimo0921@yahoo.co.jp>
2022-06-11 20:13:34 +0900

    チャット画面の編集

--- db/migrate/20220611080215_create_room_users.rb
+++ db/migrate/20220611080215_create_room_users.rb
@@ -1,8 +1,8 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-      t.string :room, null: false, foreign_key: true
-      t.string :user, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
